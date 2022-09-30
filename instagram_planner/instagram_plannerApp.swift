be68f65e351f79a2b35ed0fe018c278b1cf46e58
Vlad Lavrenkov <adlavrenkov@gmail.com>
2022-09-30 14:55:42 +0300

    init project

---
+++ instagram_planner/instagram_plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_plannerApp.swift
+//  instagram_planner
+//
+//  Created by Vlad on 30.09.22.
+//
+
+import SwiftUI
+
+@main
+struct instagram_plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
